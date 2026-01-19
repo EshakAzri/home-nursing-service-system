820d903369a4dd49430d612cb6bbf3224b365e6c
EshakAzri <eshakyaacob@gmail.com>
2026-01-19 17:29:44 +0800

    Hide sensitive data. (.env)

---
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo; // pastikan sama dengan structure projek
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication { // nama boleh ikut projek
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
