05b9b3cc22b05857058df8026c622128e3e40015
EshakAzri <eshakyaacob@gmail.com>
2026-01-22 14:51:57 +0800

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register';
-import CustomerBookingPage from './CustomerBookingPage';
+import CustomerBookingPage from './customer/CustomerBookingPage';
 import './App.css';
 
 function App() {
